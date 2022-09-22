ac618e8306629da91dc305e16aaf2aabbf9b485e
Bennett Benedict <bennybenester@gmail.com>
2022-09-22 17:17:21 +0300

    add verify route

--- src/modules/controllers/verify.controller.ts
+++ src/modules/controllers/verify.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Post, Req, Res } from "@nestjs/common";
 import { VerifyService } from "../services/verify.service";
 
 @Controller("api/verify")
@@ -7,5 +7,9 @@ export class VerifyController {
   @Get(":code")
   async verifyCrops(@Param("code") code: string) {
     await this.service.verifyCrops(code);
+  }
+  @Post()
+  async verifyCrop(@Req() req, @Res() res) {
+    await this.service.verify(req, res);
   }
 }
